1f429238ac0b71da93ba18dd2fd206ca43e7ecb2
eSqadron <kubam0906@gmail.com>
2024-05-13 17:56:24 +0200

    Minimum speed calculation 1. bugfix, minimum speed is actually calculated, modifier is no longer treated as minimum speed 2. speed PID for pos control separated from speed control

--- BrushedMotorControllerFirmware/include/calculations.h
+++ BrushedMotorControllerFirmware/include/calculations.h
@@ -2,3 +2,8 @@
 #define CALC_SPEED_CONTROL(target_speed, actual_speed) \
         ((int32_t)( CONFIG_KP_NUMERATOR_FOR_SPEED * (int32_t)(target_speed - actual_speed) / \
                     CONFIG_KP_DENOMINATOR_FOR_SPEED))
+
+
+#define CALC_SPEED_CONTROL_FOR_POS(target_speed, actual_speed) \
+        ((int32_t)( CONFIG_KP_NUMERATOR_FOR_SPEED_IN_POS * (int32_t)(target_speed - actual_speed) / \
+                    CONFIG_KP_DENOMINATOR_FOR_SPEED_IN_POS))
